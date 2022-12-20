3248c1f0d0af7c367ab1d045b88cecb89fd9d559
ndikabunder <ndikabunder@gmail.com>
2022-12-20 17:00:45 +0700

    API AUTH

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -10,6 +10,7 @@ use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Laravel\Fortify\Rules\Password;
 
 class UserController extends Controller
 {
@@ -18,12 +19,12 @@ class UserController extends Controller
         try {
             // TODO: Validate Request
             $request->validate([
-                'email' => 'required|email',
-                'password' => 'required|'
+                'email'    => 'required|email',
+                'password' => 'required'
             ]);
 
             // TODO: Get Request Email and Password
-            $credentials = request(['email'], ['password']);
+            $credentials = request(['email', 'password']);
 
             // TODO: If login failed
             if (!Auth::attempt($credentials)) {
@@ -43,12 +44,63 @@ class UserController extends Controller
 
             // TODO: Response Success Login
             return ResponseFormatter::success([
-                'access_token' =>  $tokenResult,
-                'token_type' => 'Bearer',
-                'data' => $user
+                'access_token' => $tokenResult,
+                'token_type'   => 'Bearer',
+                'data'         => $user
             ], 'Login Success');
-        } catch (Expectation $e) {
+        } catch (Expectation $error) {
+            // TODO: Error Response
             return ResponseFormatter::error('Authentication Failed');
         }
+    }
+
+    public function register(Request $request)
+    {
+        try {
+            // TODO: Validate Request
+            $request->validate([
+                'name'     => ['required', 'string', 'max:255'],
+                'email'    => ['required', 'string', 'email', 'max:255', 'unique:users'],
+                'password' => ['required', 'string', new Password],
+            ]);
+
+            // TODO: Create User
+            $user = User::create([
+                'name'     => $request->name,
+                'email'    => $request->email,
+                'password' => Hash::make($request->password)
+            ]);
+
+            // TODO: Generate Token
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+
+            // TODO: Return Success Response
+            return ResponseFormatter::success([
+                'access_token' => $tokenResult,
+                'token_type'   => 'Bearer',
+                'user'         => $user
+            ], 'Register Successfully');
+        } catch (Exception $error) {
+            // TODO: Return Failed Response
+            return ResponseFormatter::error($error->getMessage());
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        // TODO: Revoke Token
+        $token = $request->user()->currentAccessToken()->delete();
+
+        // TODO: Return Response
+        return ResponseFormatter::success($token, 'Logout Success');
+    }
+
+    public function fetch(Request $request)
+    {
+        // TODO: GET USER
+        $user = $request->user();
+
+        // TODO: Return Response
+        return ResponseFormatter::success($user, 'Fetch Success');
     }
 }
